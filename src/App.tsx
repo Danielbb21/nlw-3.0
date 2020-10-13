f223b1bff436db5e9b63412eb4215b1067487462
Daniel Bastos <danielbb2000@gmail.com>
2020-10-12 22:24:56 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import {FiArrowRight} from 'react-icons/fi';
+import logoImg from './images/Logo.svg';
+
+import Routes from './routes'
+function App() {
+  return (
+    <Routes />
+  );
+
+}
+
+export default App;
